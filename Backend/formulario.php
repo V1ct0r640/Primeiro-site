f820c2eed6b81541471b676117b76190fd27d3b1
Victor Felipe Pires <victorfelipe200514@gmail.com>
2024-11-23 01:33:06 +0000

    atualizado o formulario.php

--- Backend/formulario.php
+++ Backend/formulario.php
@@ -1,4 +1,8 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
 include 'db.php';  // Inclui a conexão com o banco de dados
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
